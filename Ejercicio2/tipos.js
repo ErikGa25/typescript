bc380889e47348356a6c3a45f1df3da1a7f8e3b8
ErikGa25 <jerik67@gmail.com>
2020-12-15 15:34:11 -0600

    se agregaron los tipos de datos de typescript

---
+++ Ejercicio2/tipos.js
@@ -0,0 +1,23 @@
+/* boolean */
+var logico = false;
+var verdadero = true;
+/* number */
+var entero = 44;
+var decimal = 45.233;
+var hexadecimal = 0xcbabaa;
+var binario = 21;
+var largo = 1567345122;
+/* string */
+var cadenas = 'mensaje#183';
+/* any */
+var cualquiera;
+cualquiera = 'string';
+cualquiera = false;
+/* null */
+var nada = null;
+/* undefined */
+var indefinido = undefined;
+/* void */
+function mensaje() {
+    console.log('Mensaje');
+}
---
+++ Ejercicio2/tipos.ts
@@ -0,0 +1,30 @@
+/* boolean */
+let logico: boolean = false
+let verdadero: boolean = true
+
+/* number */
+let entero: number = 44
+let decimal:number = 45.233
+let hexadecimal: number = 0xcbabaa
+let binario: number = 0b010101
+let largo: number = 1_567_345_122
+
+/* string */
+let cadenas = 'mensaje#183'
+
+/* any */
+let cualquiera: any
+cualquiera = 'string'
+cualquiera = false
+
+/* null */
+let nada: null = null
+
+/* undefined */
+let indefinido: undefined = undefined
+
+/* void */
+function mensaje(): void {
+    console.log('Mensaje')
+}
+
